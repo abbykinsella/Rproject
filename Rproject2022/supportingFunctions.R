933d5abcba66f937321e7215cb2768484e3c86bb
Kaitlyn <Kaitlyn@macbook-pro-5.dhcp.nd.edu>
2022-12-07 11:28:44 -0500

    functions script started

---
+++ Rproject2022/supportingFunctions.R
@@ -0,0 +1,13 @@
+setwd("~/Desktop/finalproj/Rproject/Rproject2022")
+
+# function to convert .txt files to csv 
+convertY <- function(dir) {
+  files <- list.files(dir, pattern="*.txt")
+  for(i in 1:length(files)){
+    curr <- paste(dir, files[i], sep="/")
+    csv_curr <- read.table(file = curr, sep = " ", header = TRUE)
+    csv_curr_name <- paste(dir, gsub("txt", "csv", files[i]), sep = "/")
+    write.csv(csv_curr, file = (paste(dir, gsub("txt", "csv", files[i]), sep = "/")), row.names = FALSE)
+  }
+}
+
