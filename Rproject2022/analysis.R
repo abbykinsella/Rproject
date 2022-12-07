2a406fa8d3f92f06508c3f4604b5db336cbbbfcd
Kara Clouse <klclouse1@users.noreply.github.com>
2022-12-07 11:38:48 -0500

    analysis.R

---
+++ Rproject2022/analysis.R
@@ -0,0 +1,8 @@
+setwd("~/Desktop/finalproj/Rproject/Rproject2022")
+
+# main script that calls all the functions in supportingFunctions.R
+
+# function that converts tab or space delimeted text files to comma delimited csv files
+
+# function that compiles ALL csv files into a SINGLE csv file
+# same 12 columns as original and countryX/Y and day of year in new columns
