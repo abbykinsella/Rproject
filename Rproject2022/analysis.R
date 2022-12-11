407141f7ecee5ba30278338ee5d9a26be2b8fba4
Kara Clouse <klclouse1@users.noreply.github.com>
2022-12-10 20:59:54 -0500

    worked on first two functions, started piecing them together in analysis.R

--- Rproject2022/analysis.R
+++ Rproject2022/analysis.R
@@ -1,8 +1,53 @@
 setwd("~/Desktop/finalproj/Rproject/Rproject2022")
 
-# main script that calls all the functions in supportingFunctions.R
+# set up and link
+library(ggplot2)
+library(cowplot)
+source('supportingFunctions.R')
 
+################# main script that calls all the functions in supportingFunctions.R   ########################
+
+
+###### FUNCTION CONVERT CALL ##########
 # function that converts tab or space delimeted text files to comma delimited csv files
+#   ***countryX is already in csv form so this step is not necessary
+convert_csv("countryY")
 
+
+###### FUNCTION COMPILE CALL ##########
 # function that compiles ALL csv files into a SINGLE csv file
 # same 12 columns as original and countryX/Y and day of year in new columns
+
+### mini function user input ###
+# user options
+    # 1. remove rows with NAs in any column
+    # 2. include NAs but be warned of their presence
+    # 3. include NAs but don't be warned
+user_input <- get_choice()                  ######do we need to account for malicious users >:)
+
+
+# call compile function with choice
+    # clear any previous compiltions
+if (file.exists("all.csv")) {
+  unlink("all.csv")
+}
+compile("countryX", user_input)
+compile("countryY", user_input)
+
+
+
+###### FUNCTION SUMMARIZE CALL ##########
+
+file <- "allData.csv"     # provided data, we can test with our file too 
+s <- summarize(file)
+
+
+
+
+
+####### THINGS TO DO #########
+# 1. do we have to check if they put like an invalid number in
+# 2. i don't know how to check if 2 works, if it is actually catching those NAs
+# 3. write the summary function
+# 4. answer questions from beginning of PDF
+
--- Rproject2022/supportingFunctions.R
+++ Rproject2022/supportingFunctions.R
@@ -1,9 +1,14 @@
 setwd("~/Desktop/finalproj/Rproject/Rproject2022")
 
+
+######## FUNCTION CONVERT  ##########
 # function to convert .txt files to csv 
-convertY <- function(dir) {
+convert_csv <- function(dir) {
   # get all the files from the directory
   files <- list.files(dir, pattern="*.txt")
+  
+  print("Files being converted to CSV files...")
+  
   for(i in 1:length(files)){
     # get the path of the file
     curr <- paste(dir, files[i], sep="/")
@@ -12,8 +17,12 @@ convertY <- function(dir) {
     # create csv file 
     write.csv(csv_curr, file = (paste(dir, gsub("txt", "csv", files[i]), sep = "/")), row.names = FALSE)
   }
+  
+  print("Files converted.")
 }
 
+
+######## FUNCTION COMPILE  ##########
 # function to compile all .csv files in a dir to a single file
 compile <- function(dir, toWarn){
   # get all the files in dir
@@ -34,18 +43,64 @@ compile <- function(dir, toWarn){
     # add the row to the data frame
     df <- rbind(df, curr_csv)
   }
-  # remove NAs if remove is passed to function
-  if(toWarn == "remove"){
+  
+  # user options
+  # 1. remove rows with NAs in any column
+  # 2. include NAs but be warned of their presence
+  # 3. include NAs but don't be warned
+  
+  
+  # handle NAs based on user input
+  if(toWarn == "1"){
     if(sum(is.na(df))) {
       df <- na.omit(df)
     }
   }
-  # else warn of NAs presence if warn is passed to function
-  else if(toWarn == "warn"){
+  else{
     if(sum(is.na(df))) {
-      print("Warning: NAs are present")
+      if(toWarn == "2") {
+        print("Warning: NAs are present")   # only warn if 2 is selected
+      }
     }
   }
-  write.csv(df, "all.csv", row.names=FALSE)
+  
+  # write everything to a file!
+  write.csv(df, "all.csv", append=TRUE, row.names=FALSE)
   return(df)
 }
+
+###### FUNCTION SUMMARIZE  ##########
+summarize <- function(file) {
+  # number of screens run
+  # percent of patients screened that were infected
+  # male vs female patients
+  # age distribution of patients
+  
+  summary = data.frame(matrix(ncol = 7, nrow = 0))
+  cols <- c("Num of Screens", "Patients Infected", "Male", "Female", "Age Min", "Age Max", "Age Mean")
+  colnames(summary) = cols
+
+  return(summary)
+}
+
+
+
+######## HELPER FUNCTIONS   ##########
+
+# helper function that asks user for choice regarding csv file with all the data
+get_choice <- function() {
+  print("1. Remove rows with NAs in any column")
+  print("2. Include NAs but be warned of their presence")
+  print("3. Include NAs but don't be warned")
+  choice = readline(prompt = "Enter any number : ")
+  return(choice)
+}
+
+
+
+
+
+
+
+
+
